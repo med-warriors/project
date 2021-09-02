648c0a1120d4658b2e8544ff6c22de16bc32c783
weixiao hu <weixiaoh@hawaii.edu>
2021-09-02 12:42:39 -1000

    Medicine and Supplies page
    Created a Medicine and Supplies page with simple outline. Also attach the page menu bar and app.

---
+++ app/imports/ui/pages/MedicineandSupplies.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Table, Header, Loader, Tab } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Stuffs } from '../../api/stuff/Stuff';
+import StuffItem from '../components/StuffItem';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ListStuff extends React.Component {
+
+  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the page once subscriptions have been received.
+  renderPage() {
+    return (
+      <Container>
+        <Header as="h2" textAlign="center">Medicine and Supplies</Header>
+        <Table celled>
+          <Tab panes={[
+            { menuItem: 'Medicine', render: () => <Tab.Pane>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Lot #</Table.HeaderCell>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Type</Table.HeaderCell>
+                  <Table.HeaderCell>Location</Table.HeaderCell>
+                  <Table.HeaderCell>Quantity</Table.HeaderCell>
+                  <Table.HeaderCell>Expiration Date</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+            </Tab.Pane> },
+            { menuItem: 'Supplies', render: () => <Tab.Pane>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Location</Table.HeaderCell>
+                  <Table.HeaderCell>Quantity</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+            </Tab.Pane> }]}/>
+          <Table.Body>
+            {this.props.stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
+          </Table.Body>
+        </Table>
+      </Container>
+    );
+  }
+}
+
+// Require an array of Medicine and Supplies documents in the props.
+ListStuff.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const stuffs = Stuffs.collection.find({}).fetch();
+  return {
+    stuffs,
+    ready,
+  };
+})(ListStuff);
