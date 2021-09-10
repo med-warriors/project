30aadce1bce3f3d583d6e64d090b3e6b2c69d9e6
weixiao hu <weixiaoh@hawaii.edu>
2021-09-09 14:07:12 -1000

    Update MedicineandSupplies.jsx
    sorted list

--- app/imports/ui/pages/MedicineandSupplies.jsx
+++ app/imports/ui/pages/MedicineandSupplies.jsx
@@ -71,6 +71,17 @@ export default withTracker(() => {
   const ready = subscription.ready();
   // Get the Medicine documents
   const stuffs = Stuffs.collection.find({}).fetch();
+  stuffs.sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
   return {
     stuffs,
     ready,
