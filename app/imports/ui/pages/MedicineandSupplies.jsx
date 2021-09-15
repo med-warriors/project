c7367e514dcf6013422571cc5909943182f77d96
weixiao hu <weixiaoh@hawaii.edu>
2021-09-14 20:01:14 -1000

    Update MedicineandSupplies.jsx

--- app/imports/ui/pages/MedicineandSupplies.jsx
+++ app/imports/ui/pages/MedicineandSupplies.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import { Container, Table, Header, Loader, Tab } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -8,40 +9,67 @@ import CurrentSupplies from '../components/CurrentSupplies';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Renders a table containing all of the Medicine And Supplies documents. Use <MedicineAndSuppliesItem> to render each row. */
-const MedicineAndSupplies = ({ ready, stuffs }) => ((ready) ? (
+const MedicineAndSupplies = ({ ready, dispatch, column, data, direction }) => ((ready) ? (
   <Container id={PAGE_IDS.LIST_STUFF}>
     <Header as="h2" textAlign="center">Medicine and Supplies</Header>
     <Tab panes = {[
       // eslint-disable-next-line react/display-name
       { menuItem: 'Medicine', render: () => <Tab.Pane>
-        <Table celled>
+        <Table sortable celled fixed>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Lot #</Table.HeaderCell>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Type</Table.HeaderCell>
-              <Table.HeaderCell>Location</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Expiration Date</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'lot' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'lot' })}
+              >Lot #</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'name' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'name' })}
+              >Name</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'type' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'type' })}
+              >Type</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'location' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'location' })}
+              >Location</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'quantity' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'quantity' })}
+              >Quantity</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'expirationdate' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'expirationdate' })}
+              >Expiration Date</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {stuffs.map((stuff) => <CurrentMedicine key={stuff._id} stuff={stuff}/>)}
+            {data.map((stuff) => <CurrentMedicine key={stuff._id} stuff={stuff}/>)}
           </Table.Body>
         </Table>
       </Tab.Pane> },
       // eslint-disable-next-line react/display-name
       { menuItem: 'Supplies', render: () => <Tab.Pane>
-        <Table celled>
+        <Table sortable celled fixed>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Location</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'name' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'name' })}
+              >Name</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'location' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'location' })}
+              >Location</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'quantity' ? direction : null}
+                onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'quantity' })}
+              >Quantity</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {stuffs.map((stuff) => <CurrentSupplies key={stuff._id} stuff={stuff} />)}
+            {data.map((stuff) => <CurrentSupplies key={stuff._id} stuff={stuff} />)}
           </Table.Body>
         </Table>
       </Tab.Pane> }]}/>
@@ -52,18 +80,53 @@ const MedicineAndSupplies = ({ ready, stuffs }) => ((ready) ? (
 MedicineAndSupplies.propTypes = {
   stuffs: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  column: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  direction: PropTypes.string.isRequired,
 };
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
-  // Get access to Stuff documents.
+  // Get access to Medicine and supplies documents.
   const subscription = Stuffs.subscribeStuff();
   // Determine if the subscription is ready
   const ready = subscription.ready();
-  // Get the Stuff documents and sort them by name.
-  const stuffs = Stuffs.find({}, { sort: { name: 1 } }).fetch();
+  // Get the Medicine documents and sort them by name.
+  const stuffs = Stuffs.find().fetch();
+  // Reduce the Medicine documents in sortable table cell.
+  const sortReducer = (state, action) => {
+    switch (action.type) {
+    case 'CHANGE_SORT':
+      if (state.column === action.column) {
+        return {
+          ...state,
+          data: state.data.slice().reverse(),
+          direction:
+              state.direction === 'ascending' ? 'descending' : 'ascending',
+        };
+      }
+      return {
+        column: action.column,
+        data: _.sortBy(state.data, [action.column]),
+        direction: 'ascending',
+      };
+    default:
+      throw new Error();
+    }
+  };
+  // Determine the sorted state of Medicine documents by column and direction.
+  const [state, dispatch] = React.useReducer(sortReducer, {
+    column: null,
+    data: stuffs,
+    direction: null,
+  });
+  const { column, data, direction } = state;
   return {
-    stuffs,
+    dispatch,
+    column,
+    data,
+    direction,
     ready,
   };
 })(MedicineAndSupplies);
