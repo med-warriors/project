32bc15bb28bb62e69627593dad3d86b16eb81728
karenwong-kw <77661920+karenwong-kw@users.noreply.github.com>
2021-09-16 10:22:28 -1000

    mockup  buttons added
    feature for editing profile, username

--- app/imports/ui/components/ProfileItem.jsx
+++ app/imports/ui/components/ProfileItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Grid, Header, List } from 'semantic-ui-react';
+import { Image, Grid, Header, Label, Segment, Button, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -10,26 +10,38 @@ class ProfileItem extends React.Component {
 
       <Grid id="profile-grid">
         <Grid.Row>
-          <Header as='h2' id="welcome-name" icon textAlign='center'>
-            <Header.Content> <Header as='h3' block>
-              Welcome, {this.props.profile.username}
+          <Grid.Column><Header as='h2' icon textAlign='center'>
+            <Header.Content> <Header as='h3' block textAlign='center'>
+                Welcome, {this.props.profile.username}
             </Header></Header.Content>
-          </Header>
+          </Header></Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={5}>
             <Image src={this.props.profile.image} size='large' circular/>
+            <Button animated id="edit-prof">
+              <Button.Content visible>Edit Profile</Button.Content>
+              <Button.Content hidden>
+                <Icon name='arrow right' />
+              </Button.Content>
+            </Button>
           </Grid.Column>
-          <Grid.Column width={8} floated='left'>
-            <Header id="profile-name" size={'huge'}>
-              {this.props.profile.name}
-            </Header>
-            <Header id="profile-number" as="h3">
-              ID # :{this.props.profile.idnumber}
-            </Header>
-            <Header id="profile-role" as="h3">
-              Affiliations :  {this.props.profile.role}
-            </Header>
+          <Grid.Column id="profile-info" width={9}>
+            <Segment floated='right'>
+              <Grid.Row>
+                <Header id="profile-name" size={'huge'} textAlign='left'>
+                  {this.props.profile.name}
+                </Header></Grid.Row>
+              <Grid.Row><Header id="profile-number" as="h3" textAlign='left'>
+                  ID # : <Label color='black'>{this.props.profile.idnumber}</Label>
+              </Header>
+              </Grid.Row>
+              <Grid.Row><Header id="profile-username" as="h3" textAlign='left'>
+                Username : {this.props.profile.username}
+              </Header></Grid.Row>
+              <Grid.Row><Header id="profile-role" as="h3" textAlign='left'>
+                  Affiliations : <Label circular>{this.props.profile.role}</Label>  <Label circular>staff</Label>
+              </Header></Grid.Row></Segment>
           </Grid.Column>
         </Grid.Row>
       </Grid>
