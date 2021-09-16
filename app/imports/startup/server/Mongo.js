fdd05585ce704525daf6433279a7d5cd682c1c3f
Jin <jinyan@hawaii.edu>
2021-09-15 22:59:17 -1000

    created functions for initializing and adding patients

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/StuffCollection';
+import { Patients } from '../../api/patients/PatientCollection';
+
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -13,5 +15,19 @@ if (Stuffs.count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the database with a default data document.
+function addPatients(data) {
+  console.log(`  Adding patient: ${data.date} (${data.name})`);
+  Patients.define(data);
+}
+
+// Initialize the PatientCollection if empty.
+if (Patients.count() === 0) {
+  if (Meteor.settings.defaultPatientHistory) {
+    console.log('Creating default patient data.');
+    Meteor.settings.defaultPatientHistory.map(data => addPatients(data));
   }
 }
