42f57e006abd01d134a518a1c41c7264bbe47d54
Jin <jinyan@hawaii.edu>
2021-09-15 15:35:41 -1000

    added patient collection

---
+++ app/imports/api/patients/Patients.js
@@ -0,0 +1,34 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The PatientsCollection. It encapsulates state and variable values for patients.
+ */
+class PatientsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'PatientsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      date: Date,
+      name: String,
+      email: String,
+      phone_number: Number,
+      prescription: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the PatientsCollection.
+ * @type {PatientsCollection}
+ */
+export const Stuffs = new PatientsCollection();
