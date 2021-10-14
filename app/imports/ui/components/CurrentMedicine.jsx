b759b7669361840fe8369bc01d88ded82061180e
duhkneelow <dnilo@hawaii.edu>
2021-10-13 22:57:49 -1000

    Adjusted text color in quantity column

--- app/imports/ui/components/CurrentMedicine.jsx
+++ app/imports/ui/components/CurrentMedicine.jsx
@@ -4,6 +4,13 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
+// Changes text to red, yellow, or green, based on quantity of medicine
+const getColor = (quantity) => {
+  if (quantity >= 50) return '#25A18E';
+  if (quantity > 10 && quantity < 50) return '#A18E25';
+  return '#A12358';
+};
+
 /** Renders a single row in the List Medicine table. See pages/MedicineandSupplies.jsx. */
 const CurrentMedicine = ({ medicine }) => (
   <Table.Row>
@@ -11,7 +18,7 @@ const CurrentMedicine = ({ medicine }) => (
     <Table.Cell>{medicine.name}</Table.Cell>
     <Table.Cell>{medicine.type}</Table.Cell>
     <Table.Cell>{medicine.location}</Table.Cell>
-    <Table.Cell>{medicine.quantity}</Table.Cell>
+    <Table.Cell style={{ color: getColor(medicine.quantity) }}>{medicine.quantity}</Table.Cell>
     <Table.Cell>{medicine.expirationDate.toDateString()}</Table.Cell>
     <Table.Cell>{medicine.source}</Table.Cell>
     <Table.Cell>{medicine.status}</Table.Cell>
--- app/imports/ui/components/CurrentSupplies.jsx
+++ app/imports/ui/components/CurrentSupplies.jsx
@@ -4,12 +4,19 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
+// Changes text to red, yellow, or green, based on quantity of supplies
+const getColor = (quantity) => {
+  if (quantity >= 20) return '#25A18E';
+  if (quantity > 10 && quantity < 20) return '#A18E25';
+  return '#A12358';
+};
+
 /** Renders a single row in the List Supplies table. See pages/MedicineandSupplies.jsx. */
 const CurrentSupplies = ({ supply }) => (
   <Table.Row>
     <Table.Cell>{supply.name}</Table.Cell>
     <Table.Cell>{supply.location}</Table.Cell>
-    <Table.Cell>{supply.quantity}</Table.Cell>
+    <Table.Cell style={{ color: getColor(supply.quantity) }}>{supply.quantity}</Table.Cell>
     <Table.Cell>{supply.source}</Table.Cell>
     <Table.Cell>{supply.status}</Table.Cell>
     <Table.Cell>
