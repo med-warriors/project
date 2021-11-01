839e3077518bf5fc37deac7341cb0ac598fcd407
weixiao hu <weixiaoh@hawaii.edu>
2021-11-01 11:07:53 -1000

    reassign Prescription page jsx file(3)

---
+++ app/imports/ui/components/Dispense.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Form, Grid, Header, Segment } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { defineMethod, updateMethod } from '../../api/base/BaseCollection.methods';
+import { MedicineSource } from '../../api/medSource/MedicineSourceCollection';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  patientID: String,
+  outputLocation: String,
+  notes: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
+
+const Dispense = ({ cellDispense }) => {
+
+  /*
+  // On submit, insert the data to transaction history.
+  const submitTran = (data, formRef) => {
+    const { patientID, medicine } = data;
+    // Get the current date, time, and default data.
+    const date = new Date();
+    const prescription = medicine;
+    const transact = 'Out';
+    const type = 'Medicine';
+    // Get the current employee ID number.
+    const employee = 'employee name';
+    const collectionName = TransationHistories.getCollectionName();
+    const definitionData = { date, transact, type, patientID, prescription, employee };
+    // add prescription as new transaction.
+    defineMethod.callPromise({ collectionName, definitionData })
+      .catch(error => swal('Error', error.message, 'error'))
+      .then(() => {
+        swal('Success', 'Prescription output successfully', 'success');
+        formRef.reset();
+      });
+  };
+  */
+
+  const submitMed = (fRef) => {
+    const collectionName = MedicineSource.getCollectionName();
+    const med = MedicineSource.findDoc(cellDispense[0].medId);
+    const quantity = med.quantity - cellDispense[0].prescriptionQuantity;
+    const updateData = { id: med._id, quantity };
+    // update the medicine.
+    updateMethod.callPromise({ collectionName, updateData })
+      .catch(error => swal('Error', error.message, 'error'))
+      .then(() => {
+        swal('Success', 'Item updated successfully', 'success');
+        fRef.reset();
+      });
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+
+  return (
+    <Grid container centered>
+      <Grid.Column>
+        <AutoForm ref={ref => {
+          fRef = ref;
+        }} schema={bridge} onSubmit={data => submitMed(data, fRef)}>
+          <Grid.Row>
+            <Segment>
+              <Header as="h5" textAlign="center">Patient Prescription</Header>
+              <Form.Group widths='equal'>
+                <TextField name='patientID'/>
+                <TextField name='outputLocation'/>
+              </Form.Group>
+              <LongTextField name='notes'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField />
+            </Segment>
+          </Grid.Row>
+        </AutoForm>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+// Require a document to be passed to this component.
+Dispense.propTypes = {
+  cellDispense: PropTypes.array,
+};
+
+export default withRouter(Dispense);
--- app/imports/ui/pages/Prescription.jsx
+++ app/imports/ui/pages/Prescription.jsx
@@ -1,97 +1,135 @@
 import React, { useState } from 'react';
-import { Grid, Segment, Header, Form } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField } from 'uniforms-semantic';
-import swal from 'sweetalert';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-import { TransationHistories } from '../../api/transaction/TransationHistoriesCollection';
-import { defineMethod, updateMethod } from '../../api/base/BaseCollection.methods';
-import { PAGE_IDS } from '../utilities/PageIDs';
-import PrescriptionTable from '../components/PrescriptionTable';
+import { Grid, Segment, Header, Loader, Table, Input } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { _ } from 'meteor/underscore';
 import { MedicineSource } from '../../api/medSource/MedicineSourceCollection';
-
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  patientID: String,
-  outputLocation: String,
-  notes: String,
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
+import DispenseItem from '../components/DispenseItem';
+import DispenseList from '../components/DispenseList';
+import Dispense from '../components/Dispense';
 
 /** Renders the Page for adding a document. */
-
-const Prescription = () => {
-
+const Prescription = ({ ready, medicines }) => {
+  // state functions
+  const [search, setSearch] = useState('');
   const [cellDispense, setDispense] = useState([]);
+  // variable to sort medicine
+  let medSort = [];
+  const dispenseList = [];
 
-  /*
-  // On submit, insert the data to transaction history.
-  const submitTran = (data, formRef) => {
-    const { patientID, medicine } = data;
-    // Get the current date, time, and default data.
-    const date = new Date();
-    const prescription = medicine;
-    const transact = 'Out';
-    const type = 'Medicine';
-    // Get the current employee ID number.
-    const employee = 'employee name';
-    const collectionName = TransationHistories.getCollectionName();
-    const definitionData = { date, transact, type, patientID, prescription, employee };
-    // add prescription as new transaction.
-    defineMethod.callPromise({ collectionName, definitionData })
-      .catch(error => swal('Error', error.message, 'error'))
-      .then(() => {
-        swal('Success', 'Prescription output successfully', 'success');
-        formRef.reset();
-      });
-  };
-  */
-
-  const submitMed = (fRef) => {
-    const collectionName = MedicineSource.getCollectionName();
-    const med = MedicineSource.findDoc(cellDispense[0].medId);
-    const quantity = med.quantity - cellDispense[0].prescriptionQuantity;
-    const updateData = { id: med._id, quantity };
-    // update the medicine.
-    updateMethod.callPromise({ collectionName, updateData })
-      .catch(error => swal('Error', error.message, 'error'))
-      .then(() => {
-        swal('Success', 'Item updated successfully', 'success');
-        fRef.reset();
-      });
+  // variable that lets user to type name of medicine/supply in search bar
+  const handleSearch = (e, data) => {
+    e.preventDefault();
+    // sets search state to search value
+    setSearch(data.value);
   };
 
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
-  let fRef = null;
+  // variable used to help find name of medicine
+  const medSearch = (searchItem) => {
+    // allows user to type name of medicine in lowercase
+    const lowerCase = search.toLowerCase();
+    // searches medicine item based on name
+    return searchItem.medName.toLowerCase().startsWith(lowerCase);
+  };
 
-  return (
-    <Grid id={PAGE_IDS.PRESCRIPTION} container centered>
-      <Grid.Column>
-        <Header as="h2" textAlign="center">Prescription</Header>
-        <AutoForm ref={ref => {
-          fRef = ref;
-        }} schema={bridge} onSubmit={data => submitMed(data, fRef)}>
-          <Grid.Row>
-            <Grid.Row>
-              <PrescriptionTable cellDispense={cellDispense} setDispense={setDispense}/>
+  const addDispense = (data) => {
+    // sets add Dispense state to added value
+    const updateDispense = [...cellDispense, { medId: data, prescriptionQuantity: 0 }];
+    setDispense(updateDispense);
+  };
+
+  const removeDispense = (data) => {
+    // sets add Dispense state to added value
+    setDispense(cellDispense.filter(item => item.medId !== data));
+  };
+
+  if (ready) {
+    if (search) {
+      // filters medicine items by search value and sorts them by name
+      medSort = _.sortBy(medicines.filter(medicine => medSearch(medicine)), 'medName');
+    }
+    if (cellDispense) {
+      for (let i = 0; i < cellDispense.length; i++) {
+        const med = MedicineSource.findDoc(cellDispense[i].medId);
+        dispenseList.push(med);
+      }
+    }
+  }
+
+  return ((ready) ? (
+    <Grid container centered>
+      <Segment>
+        <Grid>
+          <Grid.Row centered>
+            <Grid.Row centered>
+              <Input type='search' placeholder='Search by name' icon='search' onChange={handleSearch}/>
+              <Header as="h4" textAlign="center">Medicine & Supplies Item</Header>
             </Grid.Row>
-            <Segment>
-              <Header as="h5" textAlign="center">Patient Prescription</Header>
-              <Form.Group widths='equal'>
-                <TextField name='patientID'/>
-                <TextField name='outputLocation'/>
-              </Form.Group>
-              <LongTextField name='notes'/>
-              <SubmitField value='Submit'/>
-              <ErrorsField />
-            </Segment>
+            <Table celled color='red'>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Lot #</Table.HeaderCell>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Quantity</Table.HeaderCell>
+                  <Table.HeaderCell>location</Table.HeaderCell>
+                  <Table.HeaderCell>ExpDate</Table.HeaderCell>
+                  <Table.HeaderCell>State</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {medSort.map((inventories) => <DispenseItem
+                  key={inventories._id} inventories={inventories} addDispense={addDispense}
+                />)}
+              </Table.Body>
+            </Table>
           </Grid.Row>
-        </AutoForm>
-      </Grid.Column>
+          <Grid.Row centered>
+            <Header as="h4" textAlign="center">Dispensing List</Header>
+            <Table celled color='green'>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Lot #</Table.HeaderCell>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Quantity</Table.HeaderCell>
+                  <Table.HeaderCell>location</Table.HeaderCell>
+                  <Table.HeaderCell>ExpDate</Table.HeaderCell>
+                  <Table.HeaderCell>State</Table.HeaderCell>
+                  <Table.HeaderCell>Dispense Quantity</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {dispenseList.map((inventories) => <DispenseList
+                  key={inventories._id} inventories={inventories} removeDispense={removeDispense}/>)}
+              </Table.Body>
+            </Table>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Grid.Row>
+        <Segment color='blue'>
+          <Dispense cellDispense={cellDispense} />
+        </Segment>
+      </Grid.Row>
     </Grid>
-  );
+  ) : <Loader active>Getting data</Loader>);
+};
+
+// Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use.
+Prescription.propTypes = {
+  medicines: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-export default Prescription;
+export default withTracker(() => {
+  // Get access to Medicines documents.
+  const subscription = MedicineSource.subscribeMedicineSource();
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Medicine documents and sort them by name.
+  const medicines = MedicineSource.find({ state: 'Acted' || 'Reserves' }, { sort: { name: 1 } }).fetch();
+  return {
+    medicines,
+    ready,
+  };
+})(Prescription);
