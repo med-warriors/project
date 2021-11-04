57492a993019f872df1f768e2b3d2b14ca2dee69
jakeuyeda <jakeu@hawaii.edu>
2021-11-04 03:40:45 -1000

    merge

--- app/imports/ui/pages/ChangeRole.jsx
+++ app/imports/ui/pages/ChangeRole.jsx
@@ -61,7 +61,8 @@ const ChangeRole = (propTypes) => {
         <Header as="h2" textAlign="center">Change Role</Header>
         <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)} >
           <Segment>
-            <TextField name='userEmail'/>
+            <TextField label='User&apos;s Email' name='userEmail'/>
+            <Header as="h4" textAlign="center">Roles (Pick at least one)</Header>
             <BoolField name="doctor"/>
             <BoolField name="student"/>
             <BoolField name="admin"/>
