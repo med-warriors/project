f8e411cacf7bc306223d468edca8de5d12633c90
duhkneelow <dnilo@hawaii.edu>
2021-12-06 14:45:29 -1000

    acceptance test progress

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -1,17 +1,16 @@
-import { Selector, t } from 'testcafe';
-import { addStuffPage, editStuffPage, listStuffPage, listStuffAdminPage, manageDatabasePage, signOutPage } from './simple.page';
+import { t } from 'testcafe';
+import { manageDatabasePage, signOutPage } from './simple.page';
 import { signInPage } from './signin.page';
 import { navBar } from './navbar.component';
-import { signUpPage } from './signup.page';
+// import { signUpPage } from './signup.page';
 import { landingPage } from './landing.page';
-import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
 const adminCredentials = { username: 'admin@foo.com', password: 'changeme' };
-const newCredentials = { firstName: 'Robert', lastName: 'Johnson', username: 'robert@foo.com', employeeID: '44444444', password: 'changeme', confirmPassword: 'changeme' };
+// const newCredentials = { firstName: 'Robert', lastName: 'Johnson', username: 'robert@foo.com', employeeID: '44444444', password: 'changeme', confirmPassword: 'changeme' };
 
 fixture('matrp localhost test with default db')
   .page('http://localhost:3000');
@@ -28,44 +27,32 @@ test('Test that sign in and sign out work', async () => {
   await signOutPage.isDisplayed();
 });
 
-test('Test that sign up and sign out work', async () => {
+// commented out to prevent new credentials running every time the test runs
+/* test('Test that sign up and sign out work', async () => {
   await navBar.gotoSignupPage();
   await signUpPage.signupUser(newCredentials.firstName, newCredentials.lastName, newCredentials.username, newCredentials.employeeID, newCredentials.password, newCredentials.confirmPassword);
   await navBar.isLoggedIn(newCredentials.username);
   await navBar.logout();
   await signOutPage.isDisplayed();
-});
+}); */
 
 test('Test that user pages show up', async () => {
   await navBar.gotoSigninPage();
   await signInPage.signin(credentials.username, credentials.password);
   await navBar.isLoggedIn(credentials.username);
-  await navBar.gotoAddStuffPage();
-  await addStuffPage.isDisplayed();
-  await navBar.gotoListStuffPage();
-  await listStuffPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  await t.click(editLinks.nth(0));
-  await editStuffPage.isDisplayed();
   await navBar.logout();
   await signOutPage.isDisplayed();
+  await t.wait(1000);
 });
 
 test('Test that admin pages show up', async () => {
   await navBar.gotoSigninPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
-  await navBar.gotoAddStuffPage();
-  await addStuffPage.isDisplayed();
-  await navBar.gotoListStuffPage();
-  await listStuffPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  await t.click(editLinks.nth(0));
-  await editStuffPage.isDisplayed();
-  await navBar.gotoListStuffAdminPage();
-  await listStuffAdminPage.isDisplayed();
   await navBar.gotoManageDatabasePage();
   await manageDatabasePage.isDisplayed();
+  await t.wait(5000);
+  await navBar.logout();
+  await signOutPage.isDisplayed();
+  await t.wait(1000);
 });
