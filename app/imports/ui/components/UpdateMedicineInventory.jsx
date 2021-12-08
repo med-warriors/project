e611f890cf7ec49802c1f95b9627a38abbc7c439
weixiao hu <weixiaoh@hawaii.edu>
2021-12-07 23:29:02 -1000

    add Update log to Record page

--- app/imports/ui/components/UpdateMedicineInventory.jsx
+++ app/imports/ui/components/UpdateMedicineInventory.jsx
@@ -12,21 +12,37 @@ import {
   TextField,
 } from 'uniforms-semantic';
 import { withRouter } from 'react-router-dom';
-import { updateMethod } from '../../api/base/BaseCollection.methods';
+import { Meteor } from 'meteor/meteor';
+import { defineMethod, updateMethod } from '../../api/base/BaseCollection.methods';
 import { MedicineSource } from '../../api/medSource/MedicineSourceCollection';
+import { MedicineSourceRecord } from '../../api/medsourceRecord/MedicineSourceRecordCollection';
 
 const bridge = new SimpleSchema2Bridge(MedicineSource._schema);
 
 const UpdateMedicineInventory = ({ medicine }) => {
+
+  const submitMedRecord = (data) => {
+    const { lotNumber, medName, sourceName, quantity, acquire, cost, receiveDate, expDate, state } = data;
+    const editDate = new Date();
+    const action = 'Update';
+    const change = 'Updated the information';
+    // Get the current employee ID number.
+    const employee = Meteor.user() ? Meteor.user().username : '';
+    const collectionName = MedicineSourceRecord.getCollectionName();
+    const definitionData = { lotNumber, medName, quantity, sourceName, acquire, cost, expDate, receiveDate, state, editDate, employee, action, change };
+    defineMethod.callPromise({ collectionName, definitionData });
+  };
+
   // On submit, insert the data.
   const submit = (data) => {
-    const { supplyName, quantity, sourceName, acquire, cost, state, receiveDate, _id } = data;
+    const { lotNumber, medName, quantity, sourceName, acquire, cost, receiveDate, expDate, state, _id } = data;
     const collectionName = MedicineSource.getCollectionName();
-    const updateData = { id: _id, supplyName, quantity, sourceName, acquire, cost, receiveDate, state };
+    const updateData = { id: _id, lotNumber, medName, quantity, sourceName, acquire, cost, receiveDate, expDate, state };
     updateMethod.callPromise({ collectionName, updateData })
       .catch(error => swal('Error', error.message, 'error'))
       .then(() => {
         swal('Success', 'Supply Inventory update successfully', 'success');
+        submitMedRecord(data);
       });
   };
 
